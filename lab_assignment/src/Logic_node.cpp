34a644d32842492a33f8866cc7d402b09ad9447c
Michele Moriconi <102163784+MickyMori@users.noreply.github.com>
2023-11-15 15:03:40 +0100

    working Logic node

--- lab_assignment/src/Logic_node.cpp
+++ lab_assignment/src/Logic_node.cpp
@@ -1,17 +1,34 @@
 using namespace std;
+
+#define _USE_MATH_DEFINES
+
 #include <iostream>
 #include <ros/ros.h>
 #include <std_msgs/Int32.h>
+#include <std_msgs/Float64.h>
 #include <geometry_msgs/Twist.h>
 #include <unistd.h>
 #include <lab_assignment/Marker.h>
+#include <gazebo_msgs/LinkStates.h>
+#include <cmath>
 
+//parameters
 const double img_center = 320;
-const double gain = 0.01;
-const double target_area_size = 200*200;
+const double gain = 0.005;
+const double gain_base_cam = 10;
+const double target_area_size = 25000;
 double ang_vel = 0.8;
+double cam_vel = 1.0;
 double lin_vel = 0.2;
+//variables
 double error;
+double error_base_cam = 1;
+double cam_orientation[3];
+double base_orientation[3];
+double cam_w;
+double base_w;
+double cam_theta;
+double base_theta;
 
 class Logic
 {
@@ -19,7 +36,7 @@ private:
 	// Params
 	int marker_ids[4] = {11, 12, 13, 15};
 	int index = 0;
-	bool flag = false;
+	bool flag = true;
 	std_msgs::Int32 set_target;
 	// Create a node handle
 	ros::NodeHandle nh;
@@ -27,12 +44,16 @@ private:
 	ros::Subscriber mrk_sub;
 	ros::Publisher cmd_vel_pub;
 	ros::Publisher search_id_pub;
+	ros::Publisher cam_vel_pub;
+	ros::Subscriber link_pose_sub;
 public:
 	Logic(): nh("~")
 	{
 		mrk_sub = nh.subscribe("/rosbot/aruco_marker", 1000, &Logic::marker_callback, this);
+		link_pose_sub = nh.subscribe("/gazebo/link_states", 1000, &Logic::link_states_callback, this); 
 		
 		cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+		cam_vel_pub = nh.advertise<std_msgs::Float64>("/astra_joint_velocity_controller/command", 10);
 		search_id_pub = nh.advertise<std_msgs::Int32>("/rosbot/search_id", 10);
 	
 	}
@@ -40,29 +61,106 @@ public:
 	{
 		//use the controller to correct the position of the robot
 		controller(msg);
-		cout << marker_ids[index] << endl;
 		//search new marker
 		if(msg.id == -1) {
 			set_target.data = marker_ids[index];
 			search_id_pub.publish(set_target); //set target tramite un pub, così ne cerca solo uno, vedi 13 e 15 insieme
-			move_rosbot(0.0, ang_vel);
+			move_camera(cam_vel);
 		}
 		//if target found -> go next
 		else if(msg.id == marker_ids[index] && msg.area >= target_area_size){ //vedi se c'è un modo migliore per farlo anzichè con l'area
 			index++;
-			move_rosbot(-0.4, 0.0); //altrimenti non riesce mai a trovare il prossimo token e gira all'infinito
-			sleep(1);
-			ang_vel = 0.8;
+			move_rosbot(0.0,0.0);
+			cam_vel = 1.0;
+			flag = true;
+			error_base_cam = 1;
+			move_camera(cam_vel);
 			set_target.data = marker_ids[index];
 			search_id_pub.publish(set_target);
 		}
 		//if center -> move forward, else -> correct position	
-		else if(msg.id == marker_ids[index] && msg.center.x < (320 + 10) && msg.center.x > (320 - 10))
+		else if((msg.center.x < (img_center + 50) && msg.center.x > (img_center - 50)))
 		{
-			move_rosbot(lin_vel, 0.0);
+			move_camera(0.0);
+			if(abs(error_base_cam)<0.01)
+			{
+				move_rosbot(lin_vel, 0.0);
+			}else{
+				align_base_cam();
+				move_camera(cam_vel);
+			}
 		}
 		else{
-			move_rosbot(0.0, ang_vel);
+			move_camera(cam_vel);
+		}
+	}
+	
+	void align_base_cam(){
+		error_base_cam = cam_theta - base_theta;
+		ang_vel = error_base_cam * gain_base_cam;
+		
+		if(ang_vel > 0.25)
+			ang_vel = 0.25;
+		else if(ang_vel < -0.25)
+			ang_vel = -0.25;
+		//cout<<"ang vel: "<<ang_vel<<endl;	
+		move_rosbot(0.0, ang_vel);
+		//move_camera(-ang_vel/2);
+	}
+	
+	void link_states_callback(const gazebo_msgs::LinkStates msg){
+		int i = 0;
+		bool found = false;
+		while(true)
+		{
+			if(msg.name[i] == "rosbot::camera_link")
+			{
+				if(cam_w < 1 + 0.001 && cam_w > 1 + 0.001)
+				{
+					cam_theta = 0;
+					cam_orientation[0] = 1;
+					cam_orientation[1] = 0;
+					cam_orientation[2] = 0;
+				}else{
+					cam_w = msg.pose[i].orientation.w;
+					cam_theta = 2*acos(cam_w);
+					cam_orientation[0] = msg.pose[i].orientation.x/sin(cam_theta/2);
+					cam_orientation[1] = msg.pose[i].orientation.y/sin(cam_theta/2);
+					cam_orientation[2] = msg.pose[i].orientation.z/sin(cam_theta/2);
+					if(cam_orientation[2] < 0)
+						cam_theta = -(cam_theta-2*M_PI); 
+				}
+				
+				
+				cout<<"cam: "<<endl<<"   x: "<<cam_orientation[0]<<endl<<"   y: "<<cam_orientation[1]<<endl<<"   z: "<<cam_orientation[2]<<endl<<"   theta: "<<cam_theta<<endl;
+				if(found)
+					break;
+				found = true;
+			}
+			else if(msg.name[i] == "rosbot::base_link")
+			{
+				if(base_w < 1 + 0.001 && base_w > 1 + 0.001)
+				{
+					base_theta = 0;
+					base_orientation[0] = 1;
+					base_orientation[1] = 0;
+					base_orientation[2] = 0;
+				}else{
+					base_w = msg.pose[i].orientation.w;
+					base_theta = 2*acos(base_w);
+					base_orientation[0] = msg.pose[i].orientation.x/sin(base_theta/2);
+					base_orientation[1] = msg.pose[i].orientation.y/sin(base_theta/2);
+					base_orientation[2] = msg.pose[i].orientation.z/sin(base_theta/2);
+					if(base_orientation[2] < 0)
+						base_theta = -(base_theta-2*M_PI); 
+				}
+				
+				cout<<"base: "<<endl<<"   x: "<<base_orientation[0]<<endl<<"   y: "<<base_orientation[1]<<endl<<"   z: "<<base_orientation[2]<<endl<<"   theta: "<<base_theta<<endl;
+				if(found)
+					break;
+				found = true;
+			}
+			i++;	
 		}
 	}
 	
@@ -70,7 +168,12 @@ public:
 	void controller(const lab_assignment::Marker msg){
 		if(msg.id == marker_ids[index]) {
 			error = img_center - msg.center.x;
-			ang_vel = error * gain;
+			cam_vel = error * gain;
+			if(cam_vel>1.5){
+				cam_vel = 1.5;
+			}else if(cam_vel<-1.5)
+				cam_vel = -1.5;
+			//cout<<"cam vel: "<<cam_vel<<endl;
 		}
 	}
 
@@ -82,14 +185,21 @@ public:
 	    	cmd_vel_msg.linear.x = lin_x;
 		cmd_vel_pub.publish(cmd_vel_msg);	
 	}
+	
+	//move the camera
+	void move_camera(double vel)
+	{
+		std_msgs::Float64 cam_vel_msg;
+		cam_vel_msg.data = vel;
+		cam_vel_pub.publish(cam_vel_msg);
+	}
 };
 
 int main(int argc, char** argv)
 {
 	// Initialize the ROS node
 	ros::init(argc, argv, "logic_node");
-	cout << "Logic node starting";
-	flush(cout);
+	sleep(4);
 	
 	Logic node;
 
